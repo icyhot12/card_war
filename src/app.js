2a743b857540108133c9ebed0f688acb545985e7
icyhot12 <rmierniczek1@gmail.com>
2022-11-19 15:15:05 +0100

    add play logic

--- src/app.js
+++ src/app.js
@@ -1,31 +1,81 @@
 let deckId
+let remainingCards
+let computerScore = 0
+let meScore = 0
+
+let newDeckBtn = document.getElementById("new-deck")
+let drawCardsBtn = document.getElementById("draw-cards")
+let remainingInfo = document.getElementById("remaining")
+let computerScoreInfo = document.getElementById("computer-score")
+let meScoreInfo = document.getElementById("me-score")
+
+let cardValues = [
+    { value: "2", generalValue: "2" },
+    { value: "3", generalValue: "3" },
+    { value: "4", generalValue: "4" },
+    { value: "5", generalValue: "5" },
+    { value: "6", generalValue: "6" },
+    { value: "7", generalValue: "7" },
+    { value: "8", generalValue: "8" },
+    { value: "9", generalValue: "9" },
+    { value: "10", generalValue: "10" },
+    { value: "JACK", generalValue: "11" },
+    { value: "QUEEN", generalValue: "12" },
+    { value: "KING", generalValue: "13" },
+    { value: "ACE", generalValue: "14" }
+]
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             deckId = data.deck_id
+            handleRemaining(data.remaining)
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
 
-document.getElementById("draw-cards").addEventListener("click", () => {
+drawCardsBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            document.getElementById("image1").innerHTML =
-                `<img src=${data.cards[0].image} class="w-[120px] h-[167px]"/>`
-            document.getElementById("image2").innerHTML =
-                `<img src=${data.cards[1].image} class="w-[120px] h-[167px]"/>`
+            handleRemaining(data.remaining)
+            data.cards.map((card, index) => {
+                document.getElementById(`image${index}`).innerHTML = `<img src=${card.image} class="w-[120px] h-[167px]"/>`
+            })
+            return data.cards
+        })
+        .then(cards => {
+            let computerCard = getGeneralValue(cards[0].value)
+            let playerCard = getGeneralValue(cards[1].value)
+
+            if(computerCard > playerCard) {
+                computerScore++
+            } else if (computerCard < playerCard) {
+                meScore++
+            } else {
+                computerScore++
+                meScore++
+            }
+
+            computerScoreInfo.innerHTML = `Computer: ${computerScore}`
+            meScoreInfo.innerHTML = `Me: ${meScore}`
+
         })
 })
 
-/*
+function getGeneralValue(localValue) {
+    let tempValue
+    cardValues.map((card,index) => {
+        if(card.value === localValue){
+            tempValue = parseInt(cardValues[index].generalValue)
+        }
+    })
+    return tempValue
+}
 
-2) score
-3) computer: score
-4) me: score
-
-*/
+function handleRemaining(remainingNumber) {
+    remainingCards = remainingNumber
+    remainingInfo.innerHTML = `<p class="text-white text-lg" id="remaining">Remaining cards: ${remainingCards}</p>`
+}
